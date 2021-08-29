96ed98e6c2afbda1cf417f8966bfab7901a9f9b5
RidhikGovind <ridzman2019@gmail.com>
2021-08-29 18:09:40 +0530

    webpack initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Webpack starter pack 
+
+Repo containing just the basic code samples for starting webpack projects. 
+
+Comes with: 
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+            test: /.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+            ]
+            }
+        ]
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 8000,
+      },
+    
+
+}
