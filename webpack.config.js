42155fdc610763bbc2d8f47e2cc75d6226784eb2
RidhikGovind <ridzman2019@gmail.com>
2021-08-29 18:13:24 +0530

    added 'mode'

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,25 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+		compress: true,
+		port: 8000,
     },
-    module: {
-        rules: [
-            {
-            test: /.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-            ]
-            }
-        ]
-    },
-    devServer: {
-        static: {
-          directory: path.join(__dirname, 'public'),
-        },
-        compress: true,
-        port: 8000,
-      },
-    
-
-}
+    mode: 'development',
+};
